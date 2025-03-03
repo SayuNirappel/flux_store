7ea4d6a788c6345b9bd99399d5d55420dd998a03
SayuNirappel <sayunirappel@gmail.com>
2025-03-03 12:51:28 +0530

    inkwell updated

--- lib/view/home_screen/home_screen.dart
+++ lib/view/home_screen/home_screen.dart
@@ -1,12 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:flux_store/model/home_screen_models/category_model.dart';
 import 'package:flux_store/utils/constants/color_constants.dart';
 import 'package:flux_store/utils/constants/image_constants.dart';
 
 void main() {}
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int selectedCategory = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -27,29 +34,44 @@ class HomeScreen extends StatelessWidget {
   }
 
   Row _buildCategorySection() {
+    final List<CategoryModel> categories = [
+      CategoryModel(name: "female", icon: Icons.female),
+      CategoryModel(name: "Male", icon: Icons.male),
+      CategoryModel(name: "Accessories", icon: Icons.access_alarm),
+      CategoryModel(name: "Beauty", icon: Icons.image)
+    ];
     return Row(
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: List.generate(
           4,
-          (index) => Column(
-                spacing: 6,
-                children: [
-                  Container(
-                    padding: EdgeInsets.all(3),
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(100),
-                        border: Border.all()),
-                    child: CircleAvatar(
-                      backgroundColor: ColorConstants.kofblack,
-                      radius: 18,
-                      child: Icon(
-                        Icons.female,
-                        color: ColorConstants.white,
+          (index) => InkWell(
+                onTap: () {
+                  setState(() {
+                    selectedCategory = index;
+                  });
+                },
+                child: Column(
+                  spacing: 6,
+                  children: [
+                    Container(
+                      padding: EdgeInsets.all(3),
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(100),
+                          border: Border.all()),
+                      child: CircleAvatar(
+                        backgroundColor: selectedCategory == index
+                            ? ColorConstants.kofblack
+                            : ColorConstants.lightgrey,
+                        radius: 18,
+                        child: Icon(
+                          categories[index].icon,
+                          color: ColorConstants.white,
+                        ),
                       ),
                     ),
-                  ),
-                  Text("Women")
-                ],
+                    Text(categories[index].name)
+                  ],
+                ),
               )),
     );
   }
