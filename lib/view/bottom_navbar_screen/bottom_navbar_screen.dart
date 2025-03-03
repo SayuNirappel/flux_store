c41c7e4de8339f3e90ac3f4082bf3cc3eca11bf5
SayuNirappel <sayunirappel@gmail.com>
2025-03-03 11:01:04 +0530

    bnavbar updated but need verification

--- lib/view/bottom_navbar_screen/bottom_navbar_screen.dart
+++ lib/view/bottom_navbar_screen/bottom_navbar_screen.dart
@@ -11,31 +11,61 @@ class BottomNavbarScreen extends StatefulWidget {
 }
 
 class _BottomNavbarScreenState extends State<BottomNavbarScreen> {
+  int selectedIndex = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      extendBody: true,
       body: Container(
         color: Colors.lightGreenAccent,
       ),
-      bottomNavigationBar:
-          BottomNavigationBar(type: BottomNavigationBarType.fixed, items: [
-        BottomNavigationBarItem(
-            icon: Image.asset(ImageConstants.inactivehomeIcon),
-            activeIcon: Image.asset(ImageConstants.activehomeIcon),
-            label: ""),
-        BottomNavigationBarItem(
-            icon: Image.asset(ImageConstants.inactivesearchIcon),
-            activeIcon: Image.asset(ImageConstants.activesearchIcon),
-            label: ""),
-        BottomNavigationBarItem(
-            icon: Image.asset(ImageConstants.inactivehomeIcon),
-            activeIcon: Image.asset(ImageConstants.activehomeIcon),
-            label: ""),
-        BottomNavigationBarItem(
-            icon: Image.asset(ImageConstants.inactivehomeIcon),
-            activeIcon: Image.asset(ImageConstants.activehomeIcon),
-            label: "")
-      ]),
+      bottomNavigationBar: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.only(
+              topRight: Radius.circular(20), topLeft: Radius.circular(20)),
+          boxShadow: [
+            BoxShadow(color: Colors.black45, blurRadius: 3),
+          ],
+        ),
+        child: ClipRRect(
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(20),
+            topRight: Radius.circular(20),
+          ),
+          child: BottomNavigationBar(
+            onTap: (value) {
+              setState(() {
+                selectedIndex = value;
+              });
+            },
+            currentIndex: selectedIndex,
+            type: BottomNavigationBarType.fixed,
+            items: [
+              BottomNavigationBarItem(
+                icon: Image.asset(ImageConstants.inactivehomeIcon),
+                activeIcon: Image.asset(ImageConstants.activehomeIcon),
+                label: "",
+              ),
+              BottomNavigationBarItem(
+                icon: Image.asset(ImageConstants.inactivesearchIcon),
+                activeIcon: Image.asset(ImageConstants.activesearchIcon),
+                label: "",
+              ),
+              BottomNavigationBarItem(
+                icon: Image.asset(ImageConstants.inactivehomeIcon),
+                activeIcon: Image.asset(ImageConstants.activehomeIcon),
+                label: "",
+              ),
+              BottomNavigationBarItem(
+                icon: Image.asset(ImageConstants.inactivehomeIcon),
+                activeIcon: Image.asset(ImageConstants.activehomeIcon),
+                label: "",
+              ),
+            ],
+          ),
+        ),
+      ),
     );
+    //
   }
 }
