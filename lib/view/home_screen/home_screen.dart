a039a64c7335f6df0c8c74873827992789c81b15
SayuNirappel <sayunirappel@gmail.com>
2025-03-04 12:15:09 +0530

    scroll updated with list

--- lib/view/home_screen/home_screen.dart
+++ lib/view/home_screen/home_screen.dart
@@ -29,38 +29,82 @@ class _HomeScreenState extends State<HomeScreen> {
             padding: const EdgeInsets.only(left: 34, right: 35, top: 20),
             child: Column(
               children: [
+                //category section
                 _buildCategorySection(),
-                CarouselSlider.builder(
-                  options: CarouselOptions(
-                    height: 200,
-                    viewportFraction: 1,
-                    initialPage: 0,
-                    enableInfiniteScroll: true,
-                    reverse: false,
-                    autoPlay: true,
-                    autoPlayInterval: Duration(seconds: 6),
-                    autoPlayAnimationDuration: Duration(milliseconds: 800),
-                    autoPlayCurve: Curves.fastOutSlowIn,
-                    enlargeCenterPage: true,
-                    enlargeFactor: 0.3,
-                    scrollDirection: Axis.horizontal,
-                  ),
-                  itemCount: 15,
-                  itemBuilder: (BuildContext context, int itemIndex,
-                          int pageViewIndex) =>
-                      Container(
-                    decoration: BoxDecoration(
-                        image: DecorationImage(
-                            fit: BoxFit.cover,
-                            image: NetworkImage(
-                                "https://images.pexels.com/photos/21760959/pexels-photo-21760959/free-photo-of-people-working-on-field-near-deep-forest.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load")),
-                        borderRadius: BorderRadius.circular(20)),
-                  ),
+                SizedBox(
+                  height: 30,
+                ),
+                _buildCarouselSection(),
+                SizedBox(
+                  height: 30,
+                ),
+                Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text("Feature Products"),
+                        TextButton(onPressed: () {}, child: Text("Show all"))
+                      ],
+                    ),
+                    SingleChildScrollView(
+                      scrollDirection: Axis.horizontal,
+                      child: Row(
+                        children: [
+                          Column(
+                            children: [
+                              Container(
+                                height: 172,
+                                width: 136,
+                                decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(10),
+                                    image: DecorationImage(
+                                        fit: BoxFit.cover,
+                                        image: NetworkImage(
+                                            "https://images.pexels.com/photos/30743125/pexels-photo-30743125/free-photo-of-woman-in-green-dress-by-historical-landmark.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"))),
+                              ),
+                              Text("Turtle Neck Sweater"),
+                              Text("\$39.99")
+                            ],
+                          )
+                        ],
+                      ),
+                    )
+                  ],
                 )
               ],
             ),
           ),
         ));
+  }
+
+  Widget _buildCarouselSection() {
+    return CarouselSlider.builder(
+      options: CarouselOptions(
+        height: 168,
+        viewportFraction: 1,
+        initialPage: 0,
+        enableInfiniteScroll: true,
+        reverse: false,
+        autoPlay: true,
+        autoPlayInterval: Duration(seconds: 6),
+        autoPlayAnimationDuration: Duration(milliseconds: 800),
+        autoPlayCurve: Curves.fastOutSlowIn,
+        enlargeCenterPage: true,
+        enlargeFactor: 0.3,
+        scrollDirection: Axis.horizontal,
+      ),
+      itemCount: 15,
+      itemBuilder: (BuildContext context, int itemIndex, int pageViewIndex) =>
+          Container(
+        decoration: BoxDecoration(
+            image: DecorationImage(
+                fit: BoxFit.cover,
+                image: NetworkImage(
+                    "https://images.pexels.com/photos/21760959/pexels-photo-21760959/free-photo-of-people-working-on-field-near-deep-forest.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load")),
+            borderRadius: BorderRadius.circular(20)),
+      ),
+    );
   }
 
   Row _buildCategorySection() {
